ead1177740c97f0cc01f430079b9cb9bf20406ba
Paulo andrade <source.compu@gmail.com>
2017-10-15 23:21:55 -0500

    Correcciones basicas de configuracion.

--- gulpfile.js
+++ gulpfile.js
@@ -7,28 +7,30 @@ Email: source.compu@gmail.com
 Web: http://www.pauloandrade1.com
 ************************************************/
 
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var gutil = require('gulp-util');
-var minifyCss = require('gulp-minify-css');
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	gutil = require('gulp-util'),
+	minifyCss = require('gulp-minify-css');
 
 var paths = {
-  scripts: 'js/*.js',
-  css: 'css/*.css',
+  scripts: 'public_html/js/*.js',
+  css: 'public_html/css/*.css'
 };
 
 // Comprime los archivos javascript
 gulp.task('scripts', function() {
 	gulp.src(paths.scripts)
+	.pipe(concat('build.js'))
 	.pipe(uglify().on('error', gutil.log))
-	.pipe(gulp.dest('js/min'))
+	.pipe(gulp.dest('./public_html/js/min'))
 });
 
 // Comprime los archivos css
 gulp.task('minify-css', function() {
   return gulp.src(paths.css)
     .pipe(minifyCss({compatibility: 'ie8'}))
-    .pipe(gulp.dest('css/min'));
+    .pipe(gulp.dest('public_html/css/min'));
 });
 
 // Corre las tareas cada vez que hay cambios
